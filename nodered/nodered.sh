a6b0ebd51802cc347aed4481345f7666307092b9
Henrik Olander <henrik.olander@gmail.com>
2018-01-20 21:48:05 +0100

    Adds node red stuff

---
+++ nodered/nodered.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     $( cd -P "$( dirname "$SOURCE" )" )
+     pwd
+}
+
+BASE=$(get_script_dir)
+mkdir -p ${BASE}/data
+
+docker run --name nodered \
+	-dit \
+	-p 1880:1880 \
+	-v ${BASE}/data:/data \
+	--restart unless-stopped \
+	--link mymqtt:mqttbroker \
+	-u $(id -u):$(id -g) \
+	nodered/node-red-docker
+
